094a78114fb286c639385836eed1ce0397888ccc
ANANDKNIT <anand.sharma0510@gmail.com>
2019-08-13 23:51:51 +0530

    passing method as references between Components

--- src/App.js
+++ src/App.js
@@ -2,15 +2,49 @@ import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Anand", age: 24 },
+      { name: "Manu", age: 26 },
+      { name: "Test", age: 30 }
+    ]
+  };
+
+  switchNameHandler = (newValue) => {
+    console.log("was clicked");
+    // this.state.persons[0].name="ANAND KUMAR" //Don't Do This React will not recognise it
+    this.setState({
+      persons: [
+        { name: newValue, age: 24 },
+        { name: "MAXIMILIAN", age: 30 },
+        { name: "Test", age: 12 }
+      ]
+    });
+  };
   render() {
     return (
       <div className="App">
         <h1>hi this is my first app </h1>
-        <Person name="Anand" age="24">
+        <button 
+        onClick={this.switchNameHandler.bind(this, 'Anand!!')}
+        >Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler.bind(this, 'Change Anand!!')}
+        >
           my hobby is playing Chess
         </Person>
-        <Person name="Manu" age="26" />
-        <Person />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+          click={this.switchNameHandler}
+        />
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from "react";
 const person = (props) => {
   return (
     <div>
-      <h3>I am a Person 
+      <h3 onClick={props.click}>I am a Person 
       { ` ${props.name}`} and</h3>
       <h5> I am {`${props.age}`} years Old</h5>
       {props.children}
